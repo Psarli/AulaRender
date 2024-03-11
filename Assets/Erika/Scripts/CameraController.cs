7933aae036f53e3bf6bc5280155fa91aa5cf97d9
Psarli <ppsarli2004@gmail.com>
2024-03-11 09:37:41 -0300

    s

--- Assets/Erika/Scripts/CameraController.cs
+++ Assets/Erika/Scripts/CameraController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Transform followingTarget;
+    
+    [SerializeField] float rotationSpeed = 2.0f;
+    [SerializeField] float distance = 5.0f;
+
+    [SerializeField] float minVerticalAngle = -20.0f;
+    [SerializeField] float maxVerticalAngle = 45.0f;
+
+    [SerializeField] Vector2 framingoOffset;
+
+
+    [SerializeField] bool invertX;
+    [SerializeField] bool invertY;
+
+
+    float rotationX;
+    float rotationY;
+
+    float invertXValue;
+    float invertYValue;
+
+
+
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        //condição de inversão de camera
+        invertXValue = (invertX) ? -1 : 1; // Tipo if: if = condição ? "true : false"
+        invertYValue = (invertY) ? -1 : 1;
+
+        //INPUTS
+        //rotação da camera em X relativo ao Y do mouse
+        rotationX += Input.GetAxis("Mouse Y") * invertYValue * rotationSpeed;
+
+        rotationX = Mathf.Clamp(rotationX, minVerticalAngle, maxVerticalAngle);//restrição de rptação de camera
+
+        rotationY += Input.GetAxis("Mouse X") * invertXValue * rotationSpeed;
         
+        var targetRotation = Quaternion.Euler(rotationX, rotationY, 0.0f);
+
+        var focusPosition = followingTarget.position + (Vector3)framingoOffset;
+
+        transform.position = focusPosition - targetRotation * new Vector3(0.0f, 0.0f, distance);
+        transform.rotation = targetRotation;
+
+
     }
 }
